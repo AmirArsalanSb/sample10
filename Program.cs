5d6b002493bbb1204fc6bddac51543aed79d1fed
amir <amir6665aqq@gmail.com>
2022-03-11 13:16:58 +0330

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿for (int i = 0; i < 4; i++)
+{
+    for (int x = 4; x > i+1 ; x--)
+    {
+        Console.Write(" ");
+    }
+    for (int y = 0; y <=i*2; y++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
